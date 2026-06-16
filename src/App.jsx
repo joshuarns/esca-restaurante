aaa6b7375388ef78e133d21a7ebdc36b07102a0b
Joshuarns <Joshua13375@gmail.com>
2026-06-15 21:02:13 -0600

    feat: remove announcement modal on page load

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import FeaturedSection from './components/FeaturedSection'
 import CavaSection from './components/CavaSection'
 import SideNav from './components/SideNav'
 import ReservationsPanel from './components/ReservationsPanel'
-import AnnouncementModal from './components/AnnouncementModal'
 
 export default function App() {
   return (
@@ -15,7 +14,6 @@ export default function App() {
       <CavaSection />
       <SideNav />
       <ReservationsPanel />
-      <AnnouncementModal />
     </div>
   )
 }
